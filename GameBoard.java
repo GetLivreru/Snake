f31f7c70a91c2b535666c840f095eb203ccbbf15
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-02-04 16:38:40 +0600

    add matrix size

--- GameBoard.java
+++ GameBoard.java
@@ -2,5 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameBoard extends JPanel {
-    // Ваш код для отображения игрового поля
+    // Ваш код для отображения игрового поля   
+    private int[][] matrix;
+    public GameBoard(int weight,int height){
+        matrix = new int[weight][height];
+        initializeMatrix(500);
+    }
+    
+    private void initializeMatrix(int value) {
+        // implementation of the method
+        for(int i = 0 ;i<matrix.length;i++){
+            for(int j = 0;j<matrix.length;j++){
+                matrix[i][j] = value;
+            }
+        }
+    }
 }
--- SnakeGame.java
+++ SnakeGame.java
@@ -1,5 +1,13 @@
+import javax.swing.JFrame;
+
 public class SnakeGame {
     public static void main(String[] args) {
         // Ваш код запуска игры
+        JFrame jFrame = new JFrame();
+        GameBoard gameBoard = new GameBoard(500,500);
+        jFrame.add(gameBoard);
+        jFrame.setSize(400,400);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
     }
 }
